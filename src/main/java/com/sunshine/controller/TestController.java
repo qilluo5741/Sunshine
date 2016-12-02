ffc362dda3889796a179359723df2570d1312fe4
weimeilayer <weimeilayer@163.com>
2016-12-02 17:45:32 +0800

    Omat Maven hanke

--- src/main/java/com/sunshine/controller/TestController.java
+++ src/main/java/com/sunshine/controller/TestController.java
@@ -1,13 +1,10 @@
 package com.sunshine.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.sunshine.entity.TestInfo;
 import com.sunshine.entityDto.Result;
 import com.sunshine.service.TestService;
