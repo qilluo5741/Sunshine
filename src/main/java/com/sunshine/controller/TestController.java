5299a30d822bd5fabe7bc656db552d9b27318a84
weimeilayer <weimeilayer@163.com>
2016-12-05 09:37:12 +0800

    master1

--- src/main/java/com/sunshine/controller/TestController.java
+++ src/main/java/com/sunshine/controller/TestController.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.sunshine.entityDto.Result;
 import com.sunshine.service.FileService;
 import com.sunshine.service.TestService;
